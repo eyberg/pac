dd9fb988ee1bdb2b5e6d4c75e8e71560f211bd23
ian <ian@hypatia.(none)>
2009-04-01 11:18:35 -0500

    adding in volume creation/attaching to new ec2 instance

--- EC2Wrap.rb
+++ EC2Wrap.rb
@@ -5,17 +5,25 @@ class EC2Wrap
   include Colorify
 
   def provision
-
     hash = File.open('ec2.yml') do |f| YAML.load f end
 
     ec2 = EC2::Base.new(:access_key_id => hash["access_key"], :secret_access_key => hash["secret_key"])
 
-    # ubuntu jaunty jakolope
+    # create ubuntu jaunty jakolope instance
     ni = ec2.run_instances(:image_id => "ami-b31ff8da")
     rid =  ni["reservationId"]
-
     instance = ec2.describe_instances(:reservationID => rid)
-    puts colorBlue instance.reservationSet.item[0].instancesSet.item[0].dnsName
+
+    # create volume
+    vol = ec2.create_volume( :availability_zone => "us-east-1c", :size => "5" )
+
+    dnsname = instance.reservationSet.item[0].instancesSet.item[0].dnsName
+    instanceid = instance.reservationSet.item[0].instancesSet.item[0].instanceId
+
+    #attach
+    ec2.attach_volume( :volume_id => vol.volumeId, :instance_id => instanceid, :device => '/dev/sdh')
+
+    puts colorBlue dnsname
   end
 
 end
